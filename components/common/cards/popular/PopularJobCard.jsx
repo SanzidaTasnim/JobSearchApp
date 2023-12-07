a463c102ec34a0875407e1a3a2e2fc2b68894537
SanzidaTasnim <tasnim5sanzida@gmail.com>
2023-12-07 15:47:08 +0600

    fetching data problem

---
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -0,0 +1,16 @@
+import { View, Text, TouchableOpacity, Image } from 'react-native'
+
+import styles from './popularjobcard.style'
+
+const PopularJobCard = ({ item, selectedJob, handleCardPress }) => {
+  return (
+    <TouchableOpacity
+      style={styles.container(selectedJob, item)}
+      onPress={() => handleCardPress( item )}
+    >
+
+    </TouchableOpacity>
+  )
+}
+
+export default PopularJobCard
